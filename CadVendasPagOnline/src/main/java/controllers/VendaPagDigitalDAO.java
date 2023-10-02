a7222484ea2073f483a9b58f54ee5fc4a8f47bc6
João Paulo Godinho <j_paulo_g@hotmail.com>
2023-10-02 01:03:27 -0300

    adicionando projeto cadVendasPagOnline

---
+++ CadVendasPagOnline/src/main/java/controllers/VendaPagDigitalDAO.java
@@ -0,0 +1,115 @@
+package controllers;
+
+import java.util.*;
+
+import models.VendaPagDigital;
+
+import java.sql.*;  
+  
+public class VendaPagDigitalDAO {  
+  
+	
+    public static Connection obterConexao(){ 
+
+        Connection conexao=null;
+        
+        try{  
+        	
+		      String jdbcUrl = "jdbc:postgresql://localhost:5432/loja";
+		      String usuario = "postgres";
+		      String senha = "postgres";
+
+		      Class.forName("org.postgresql.Driver");
+		      conexao = DriverManager.getConnection(jdbcUrl, usuario, senha);
+
+            
+        }catch(Exception e){
+        	
+        	System.out.println(e);
+        }
+        
+        return conexao;  
+    }  
+    
+    public static int inserir(VendaPagDigital v){  
+    	
+        int status = 0;
+        
+        try{  
+        	
+            Connection con = VendaPagDigitalDAO.obterConexao();
+            
+            PreparedStatement ps = con.prepareStatement("INSERT INTO vendas_pag_digital (cliente, cliente_email, produto, valor_pago, tipo_pagamento, tipo_entrega, status_pagamento) VALUES (?, ?, ?, ?, ?, ?, ?)");  
+            ps.setString(1, v.getCliente());
+            ps.setString(2, v.getClienteEmail());
+            ps.setString(3, v.getProduto());
+            ps.setDouble(4, v.getValorPago());
+            ps.setString(5, v.getTipoPagamento());
+            ps.setString(6, v.getTipoEntrega());
+            ps.setString(7, v.getStatusPagamento());
+              
+            status = ps.executeUpdate();  
+              
+            con.close(); 
+            
+        }catch(Exception ex){
+        	
+        	ex.printStackTrace();
+        }  
+          
+        return status;  
+    }  
+    
+      
+    
+    
+    
+    public static List<VendaPagDigital> listarTodas(){ 
+    	
+        List<VendaPagDigital> listaVendasPagDigital = new ArrayList<VendaPagDigital>();  
+          
+        try{  
+        	
+            Connection con = VendaPagDigitalDAO.obterConexao();  
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM vendas_pag_digital ORDER BY id ASC");  
+
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+            	
+            	String valorPagoTxt = rs.getString("valor_pago");
+            	double valor = Double.parseDouble(valorPagoTxt);  
+            	
+            	
+                VendaPagDigital v = new VendaPagDigital();
+                
+                v.setId(rs.getInt("id"));  
+                v.setCliente(rs.getString("cliente"));
+                v.setClienteEmail(rs.getString("cliente_email"));
+                v.setProduto(rs.getString("produto"));
+                v.setValorPago(valor);
+                v.setTipoPagamento(rs.getString("tipo_pagamento"));
+                v.setTipoEntrega(rs.getString("tipo_entrega"));
+                v.setStatusPagamento(rs.getString("status_pagamento"));  
+                                
+                listaVendasPagDigital.add(v);  
+            }  
+            
+            con.close();
+            
+        }catch(Exception e){
+        	
+        	e.printStackTrace();
+        }  
+          
+        return listaVendasPagDigital;  
+    }
+    
+    // adicionado para uso pela EL
+    public List<VendaPagDigital> getListarTodas(){ 
+    	
+        return VendaPagDigitalDAO.listarTodas();
+    }
+    
+    
+}  
---
+++ CadVendasPagOnline/src/main/java/models/VendaPagDigital.java
@@ -0,0 +1,88 @@
+package models;
+
+public class VendaPagDigital {
+
+
+	private int id;
+	private String cliente;
+	private String clienteEmail;
+	private String produto;
+	private double valorPago;
+	private String tipoPagamento;
+	private String tipoEntrega;
+	private String statusPagamento;
+	
+
+
+	// construtor sem argumentos
+	public VendaPagDigital() {
+		
+	}
+
+	
+	// m�todos get e set para todos os atributos
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getClienteEmail() {
+		return clienteEmail;
+	}
+
+	public void setClienteEmail(String clienteEmail) {
+		this.clienteEmail = clienteEmail;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public double getValorPago() {
+		return valorPago;
+	}
+
+	public void setValorPago(double valorPago) {
+		this.valorPago = valorPago;
+	}
+
+	public String getTipoPagamento() {
+		return tipoPagamento;
+	}
+
+	public void setTipoPagamento(String tipoPagamento) {
+		this.tipoPagamento = tipoPagamento;
+	}
+
+	public String getTipoEntrega() {
+		return tipoEntrega;
+	}
+
+	public void setTipoEntrega(String tipoEntrega) {
+		this.tipoEntrega = tipoEntrega;
+	}
+
+	public String getStatusPagamento() {
+		return statusPagamento;
+	}
+
+	public void setStatusPagamento(String statusPagamento) {
+		this.statusPagamento = statusPagamento;
+	}
+				
+}
